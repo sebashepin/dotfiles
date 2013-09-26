0c61440ea9d77e05a8229e7aa7cf98aaadde28a1
Sebastian <sebashepin@gmail.com>
2013-09-26 14:11:18 -0500

    Fix bugs on install script, remove bash_history

--- install.sh
+++ install.sh
@@ -2,23 +2,23 @@
 apt-get -y wmii vim rxvt-unicode nitrogen fortune-mod fortunes-off chromium-browser ncdu sshfs
 wget http://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip
 unzip EnvyCodeR-PR7.zip
-mkdir ~/.fonts
-cp Envy\ Code\ R\ PR7\/*.ttf ~/.fonts/
+mkdir ../.fonts
+cp Envy\ Code\ R\ PR7\/*.ttf ../.fonts/
 fc-cache
-fc-cache ~/.fonts/
+fc-cache ../.fonts/
 rm -rf Envy\ Code\ R\ PR7\
 rm EnvyCodeR-PR7.zip
-ln -sf ~/dotfiles/bash/.bash* ~/
-ln -sf ~/dotfiles/.gitconfig ~/
-ln -sf ~/dotfiles/.vimrc ~/
+ln -sf $(pwd)/bash/.bash* ~/
+ln -sf $(pwd)/.gitconfig ~/
+ln -sf $(pwd)/.vimrc ~/
 rm -rf ~/.vim
-ln -sf ~/dotfiles/.vim ~/
+ln -sf $(pwd)/.vim ~/
 rm -rf ~/.wmii
-ln -sf ~/dotfiles/.wmii ~/
-ln -sf ~/dotfiles/.Xdefaults ~/
+ln -sf $(pwd)/.wmii ~/
+ln -sf $(pwd)/.Xdefaults ~/
 mv /usr/bin/x-terminal-emulator /usr/bin/x-terminal-emulatorbck
-ln -sf x-terminal-emulator /usr/bin/x-terminal-emulator
+ln -sf $(pwd)/x-terminal-emulator /usr/bin/x-terminal-emulator
 chmod +x /usr/bin/x-terminal-emulator
-ln -sf ~/dotfiles/.xmodmaprc ~/
+ln -sf $(pwd)/.xmodmaprc ~/
 echo "Don't forget to download Sublime Text 3!"
 echo "And download some wallpapers!"
