72a359b4963a1502d36e97d3df08b47d783fdb5b
Sebastian <sebashepin@gmail.com>
2013-10-04 16:03:29 -0500

    Add no need to run install.sh as root anymore
    Change the install script to that only the commands
    that need root permission to run ask for it.

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #/bin/bash
-apt-get install -y wmii vim rxvt-unicode nitrogen fortune-mod fortunes-off chromium-browser ncdu sshfs
+sudo apt-get install -y wmii vim rxvt-unicode nitrogen fortune-mod fortunes-off chromium-browser ncdu sshfs
 wget http://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip
 unzip -q EnvyCodeR-PR7.zip
 mkdir -p ../.fonts
-cp Envy\ Code\ R\ PR7\/*.ttf $(pwd)/../.fonts/
-fc-cache
-fc-cache ../.fonts/
-rm -rf Envy\ Code\ R\ PR7\
+sudo cp Envy\ Code\ R\ PR7\/*.ttf $(pwd)/../.fonts/
+sudo fc-cache
+sudo fc-cache ../.fonts/
+rm -rf Envy\ Code\ R\ PR7
 rm EnvyCodeR-PR7.zip
 ln -sf $(pwd)/bash/.bash* $(pwd)/../
 ln -sf $(pwd)/.gitconfig $(pwd)/../
@@ -16,9 +16,9 @@ ln -sf $(pwd)/.vim $(pwd)/../
 rm -rf $(pwd)/../.wmii
 ln -sf $(pwd)/.wmii $(pwd)/../
 ln -sf $(pwd)/.Xdefaults $(pwd)/../
-mv /usr/bin/x-terminal-emulator /usr/bin/x-terminal-emulatorbck
-ln -sf $(pwd)/x-terminal-emulator /usr/bin/x-terminal-emulator
-chmod +x /usr/bin/x-terminal-emulator
+sudo mv /usr/bin/x-terminal-emulator /usr/bin/x-terminal-emulatorbck
+sudo ln -sf $(pwd)/x-terminal-emulator /usr/bin/x-terminal-emulator
+sudo chmod +x /usr/bin/x-terminal-emulator
 ln -sf $(pwd)/.xmodmaprc $(pwd)/../
 echo "Don't forget to download Sublime Text 3!"
 echo "And download some wallpapers!"
